89e325bfa67b2e990bb1e64c0db929da51ef31fa
zhangyunlei <393181024@qq.com>
2019-12-23 14:10:34 +0800

    优化

--- src/main/java/swagger/Start.java
+++ src/main/java/swagger/Start.java
@@ -1,12 +1,14 @@
 package swagger;
 
+//import com.spring4all.swagger.EnableSwagger2Doc;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@EnableSwagger2Doc
+@EnableSwagger2Doc//要生成文档就用这个 swagger 开启注解,这个注解功能更强大且也能启用 swagger
 //@EnableSwagger2
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Start {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication();
--- src/main/java/swagger/controller/UserController.java
+++ src/main/java/swagger/controller/UserController.java
@@ -1,9 +1,18 @@
 package swagger.controller;
 
 
-//@RestController
-//@RequestMapping(value = "/user", produces =  MediaType.APPLICATION_JSON_VALUE)
-//@Api(value = "用户信息查询", description = "用户基本信息操作API", tags = "UserController", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import swagger.util.MakeSwaggerUtil;
+
+@RestController
+@RequestMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(value = "用户信息查询", description = "用户基本信息操作API", tags = "UserController", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 //
 //    @ApiOperation(value = "/getUser", notes = "根据姓名查询用户信息")
@@ -22,5 +31,15 @@ public class UserController {
 //    public User addUser(@RequestBody User user){
 //        return user;
 //    }
+
+//    @Autowired
+//    MakeSwaggerUtil makeSwaggerUtil;
+//
+//    @RequestMapping(value = "demo")
+//    @ResponseBody
+//    public String run() {
+//        return makeSwaggerUtil.makeAPI();
+//    }
+
 
 }
