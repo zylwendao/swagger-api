440dabc6bceb7ea8fb7732f7ffd59dafa23f50e8
zhangyunlei <393181024@qq.com>
2019-12-24 19:49:10 +0800

    优化

--- src/main/java/swagger/controller/UserController.java
+++ src/main/java/swagger/controller/UserController.java
@@ -2,11 +2,8 @@ package swagger.controller;
 
 
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
