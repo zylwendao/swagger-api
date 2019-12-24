aa60467625118d6c0f1b225a8dea8ceab0a61a61
zhangyunlei <393181024@qq.com>
2019-12-24 19:48:27 +0800

    优化

--- src/main/java/swagger/controller/UserController.java
+++ src/main/java/swagger/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import swagger.util.MakeSwaggerUtil;
 
 @RestController
 @RequestMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
